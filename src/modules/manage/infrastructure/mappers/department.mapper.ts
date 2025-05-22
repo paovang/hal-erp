616b1d67f8763d8cb3baf93e3d96884426cef276
paovang <vangpaovang@gmail.com>
2025-05-22 17:52:11 +0700

    update generate code

--- src/modules/manage/infrastructure/mappers/department.mapper.ts
+++ src/modules/manage/infrastructure/mappers/department.mapper.ts
@@ -4,16 +4,8 @@ import { DepartmentId } from '../../domain/value-objects/department-id.vo';
 import { Timezone } from '@src/common/domain/value-objects/timezone.vo';
 import { DateFormat } from '@src/common/domain/value-objects/date-format.vo';
 import moment from 'moment-timezone';
-import { CodeGeneratorUtil } from '@src/common/utils/code-generator.util';
-import { GENERTE_CODE_SERVICE } from '@src/common/constants/inject-key.const';
-import { Inject } from '@nestjs/common';
 
 export class DepartmentDataAccessMapper {
-  constructor(
-    @Inject(GENERTE_CODE_SERVICE)
-    private readonly _generateCode: CodeGeneratorUtil,
-  ) {}
-
   toOrmEntity(departmentEntity: DepartmentEntity): DepartmentOrmEntity {
     const now = moment.tz(Timezone.LAOS).format(DateFormat.DATETIME_FORMAT);
     const id = departmentEntity.getId();
@@ -22,15 +14,6 @@ export class DepartmentDataAccessMapper {
     if (id) {
       mediaOrmEntity.id = id.value;
     } else {
-      // const code = await this._generateCode.generateUniqueCode(
-      //   6,
-      //   async (code: string) => {
-      //     // ถ้ามีอยู่แล้ว -> return false เพื่อให้ generate ใหม่
-      //     const exists = await this._generateCode.existsByCode(code);
-      //     return !exists;
-      //   },
-      //   'DEP',
-      // );
       mediaOrmEntity.code = 'DP-093';
     }
     mediaOrmEntity.name = departmentEntity.name;
