0bc144118afeffc3a52ae4593860a94b9b6caf6c
phoud <phoud.dev.20@gmail.com>
2025-08-14 14:35:38 +0700

    update

--- src/common/utils/server/send-data.uitl.ts
+++ src/common/utils/server/send-data.uitl.ts
@@ -28,7 +28,6 @@ export async function sendApprovalRequest(
   }
 
   console.log('object', type);
-  console.log('title', titles);
 
   const send_data_to_approval = {
     source_request_id: Number(user_approval_step_id),
