7baa48b445bc3d27a26a1ad186ed6da88c7cafb4
phetaibtc <phetaibtc@gmail.com>
2025-12-01 10:25:20 +0700

    update

--- src/modules/reports/application/queries/reportCompany/handlers/report-company.query.handler.ts
+++ src/modules/reports/application/queries/reportCompany/handlers/report-company.query.handler.ts
@@ -4,6 +4,7 @@ import { ResponseResult } from '@src/common/infrastructure/pagination/pagination
 import { Inject } from '@nestjs/common';
 import { REPORT_COMPANY_REPOSITORY } from '../../../constants/inject-key.const';
 import { IReportCompanuRepository } from '@src/modules/reports/domain/ports/output/company-repository.interface';
+import { CompanyQueryDto } from '@src/modules/manage/application/dto/query/company-query.dto';
 
 @QueryHandler(GetReportCompanyQuery)
 export class GetReportCompanyQueryHandler
@@ -14,7 +15,10 @@ export class GetReportCompanyQueryHandler
     private readonly _readRepo: IReportCompanuRepository,
   ) {}
 
-  async execute(query: GetReportCompanyQuery): Promise<ResponseResult<any>> {
-    return await this._readRepo.reportCompany(query.manager);
+  async execute(
+    query: GetReportCompanyQuery,
+    dto: CompanyQueryDto,
+  ): Promise<ResponseResult<any>> {
+    return await this._readRepo.reportCompany(query.manager, dto);
   }
 }
--- src/modules/reports/domain/ports/input/report-company-order-domain-service.interface.ts
+++ src/modules/reports/domain/ports/input/report-company-order-domain-service.interface.ts
@@ -1,5 +1,6 @@
+import { CompanyQueryDto } from '@src/modules/manage/application/dto/query/company-query.dto';
 import { EntityManager } from 'typeorm';
 
 export interface IReportCompanyServiceInterface {
-  reportCompany(manager?: EntityManager): Promise<any>;
+  reportCompany(dto: CompanyQueryDto, manager?: EntityManager): Promise<any>;
 }
