04bbeb59411d37929e156ce9e991ce9111559436
phetaibtc <phetaibtc@gmail.com>
2025-12-01 15:22:24 +0700

    update filter only status pedding

--- src/modules/reports/infrastructure/repositories/reportCompany/read.repository.ts
+++ src/modules/reports/infrastructure/repositories/reportCompany/read.repository.ts
@@ -24,6 +24,12 @@ export class ReportReadCompanyRepository implements IReportCompanuRepository {
       .leftJoinAndSelect('budget_accounts.increase_budgets', 'increase_budgets')
       .leftJoinAndSelect('budget_accounts.budget_items', 'budget_items')
       .leftJoinAndSelect(
+        'company.documents',
+        'documents',
+        'documents.status = :status',
+        { status: 'pending' },
+      )
+      .leftJoinAndSelect(
         'budget_items.increase_budget_detail',
         'increase_budget_detail',
       )
