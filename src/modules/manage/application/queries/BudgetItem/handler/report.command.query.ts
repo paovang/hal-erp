0acfff637ca15ab87bd8d2de55ee05196742597b
Repository Phoud <repository.phou991@gmail.com>
2025-12-10 10:43:37 +0700

    update

--- src/modules/manage/application/queries/BudgetItem/handler/report.command.query.ts
+++ src/modules/manage/application/queries/BudgetItem/handler/report.command.query.ts
@@ -6,7 +6,7 @@ import { Inject } from '@nestjs/common';
 import { BudgetItemEntity } from '@src/modules/manage/domain/entities/budget-item.entity';
 import { IReadBudgetItemRepository } from '@src/modules/manage/domain/ports/output/budget-item-repository.interace';
 import { UserContextService } from '@src/common/infrastructure/cls/cls.service';
-import { DepartmentUserOrmEntity } from '@src/common/infrastructure/database/typeorm/department-user.orm';
+// import { DepartmentUserOrmEntity } from '@src/common/infrastructure/database/typeorm/department-user.orm';
 import { CompanyUserOrmEntity } from '@src/common/infrastructure/database/typeorm/company-user.orm';
 
 @QueryHandler(GetReportQuery)
@@ -26,12 +26,12 @@ export class GetReportQueryHandler
 
     const user_id = user?.id;
 
-    const departmentUser = await query.manager.findOne(
-      DepartmentUserOrmEntity,
-      {
-        where: { user_id: user_id },
-      },
-    );
+    // const departmentUser = await query.manager.findOne(
+    //   DepartmentUserOrmEntity,
+    //   {
+    //     where: { user_id: user_id },
+    //   },
+    // );
 
     const company_user = await query.manager.findOne(CompanyUserOrmEntity, {
       where: {
@@ -41,13 +41,13 @@ export class GetReportQueryHandler
 
     const company_id = company_user?.company_id ?? undefined;
     const roles = user?.roles?.map((r: any) => r.name) ?? [];
-    const department_id = departmentUser?.department_id ?? null;
+    // const department_id = departmentUser?.department_id ?? null;
     return await this._readRepo.report(
       query.query,
       query.manager,
       company_id,
       roles,
-      department_id || undefined,
+      // department_id || undefined,
     );
   }
 }
