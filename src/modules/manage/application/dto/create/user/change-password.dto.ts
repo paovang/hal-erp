21bd8ffed8c8b604ee0805ce9b196e3ce36ebe71
Repository Phoud <repository.phou991@gmail.com>
2025-12-10 08:32:46 +0700

    update

--- src/modules/manage/application/dto/create/user/change-password.dto.ts
+++ src/modules/manage/application/dto/create/user/change-password.dto.ts
@@ -3,13 +3,13 @@ import { IsNotEmpty, IsString, Length } from 'class-validator';
 import { i18nValidationMessage } from 'nestjs-i18n';
 
 export class ChangePasswordDto {
-  @ApiProperty()
-  @IsNotEmpty({ message: i18nValidationMessage('validation.IS_NOT_EMPTY') })
-  @IsString({ message: i18nValidationMessage('validation.IS_STRING') })
-  @Length(6, 255, {
-    message: i18nValidationMessage('validation.PASSWORD_LENGTH'),
-  })
-  readonly old_password: string;
+  // @ApiProperty()
+  // @IsNotEmpty({ message: i18nValidationMessage('validation.IS_NOT_EMPTY') })
+  // @IsString({ message: i18nValidationMessage('validation.IS_STRING') })
+  // @Length(6, 255, {
+  //   message: i18nValidationMessage('validation.PASSWORD_LENGTH'),
+  // })
+  // readonly old_password: string;
 
   @ApiProperty()
   @IsNotEmpty({ message: i18nValidationMessage('validation.IS_NOT_EMPTY') })
