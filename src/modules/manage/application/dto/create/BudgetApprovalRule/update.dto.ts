b7bcb567f088f7eda3b3a4b4be0a516e25fdecc4
Repository Phoud <repository.phou991@gmail.com>
2025-12-10 17:17:02 +0700

    update

--- src/modules/manage/application/dto/create/BudgetApprovalRule/update.dto.ts
+++ src/modules/manage/application/dto/create/BudgetApprovalRule/update.dto.ts
@@ -1,6 +1,6 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateBudgetApprovalRuleDto } from './create.dto';
-
+// rest
 export class UpdateBudgetApprovalRuleDto extends PartialType(
   CreateBudgetApprovalRuleDto,
 ) {}
