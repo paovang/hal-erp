e20502ee3db9e948c779da818e3bb4b9085df635
phetaibtc <phetaibtc@gmail.com>
2026-04-21 11:25:22 +0700

    update

--- src/modules/manage/infrastructure/repositories/purchaseOrder/read.repository.ts
+++ src/modules/manage/infrastructure/repositories/purchaseOrder/read.repository.ts
@@ -277,10 +277,23 @@ export class ReadPurchaseOrderRepository
           });
         }
       } else {
-        if (type && (type = PurchaseRequestType.only_user)) {
-          query.andWhere('document_approver.user_id = :user_id', {
-            user_id,
-          });
+        switch (type) {
+          case PurchaseRequestType.only_user:
+            query.andWhere('document_approver.user_id = :user_id', {
+              user_id,
+            });
+            break;
+
+          case PurchaseRequestType.all:
+            query.andWhere(
+              `departments.id IN (
+          SELECT du.department_id 
+          FROM department_users du 
+          WHERE du.user_id = :user_id
+        )`,
+              { user_id },
+            );
+            break;
         }
       }
     }
