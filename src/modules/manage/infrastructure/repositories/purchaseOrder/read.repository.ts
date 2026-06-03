05dcad0b3e93665ea875410767cb9cb9a1ac16f5
phetaibtc <phetaibtc@gmail.com>
2026-06-03 14:43:16 +0700

    update

--- src/modules/manage/infrastructure/repositories/purchaseOrder/read.repository.ts
+++ src/modules/manage/infrastructure/repositories/purchaseOrder/read.repository.ts
@@ -114,12 +114,12 @@ export class ReadPurchaseOrderRepository
         `(
           purchase_orders.po_number ILIKE :search OR
           documents.title ILIKE :search OR documents.title ILIKE :search Or products.name ILIKE :search Or vendors.name ILIKE :search OR
-          CAST((
-            SELECT SUM(COALESCE(poi.total_in_lak, 0) + COALESCE(poi.vat_in_lak, 0))
-            FROM purchase_order_items poi
-            WHERE poi.purchase_order_id = purchase_orders.id
-              AND poi.deleted_at IS NULL
-          ) AS TEXT) ILIKE :search
+         CAST((
+          SELECT SUM(poi.total_in_lak)
+          FROM purchase_order_items poi
+          WHERE poi.purchase_order_id = purchase_orders.id
+            AND poi.deleted_at IS NULL
+        ) AS TEXT) ILIKE :search
         )`,
         { search: `%${query.search}%` },
       );
